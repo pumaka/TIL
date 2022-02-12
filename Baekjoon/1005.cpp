87621199f05d7057a65024e98f0b7a751fe7fc9c
pumaka <pumaka123@gmail.com>
2022-02-12 14:19:25 +0900

    1005

---
+++ Baekjoon/1005.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+int main(){
+    int T, N, K, time;
+    vector<int> v;
+    int start, end;
+    vector<int> x;
+    vector<vector<int>> f;
+    int vic;
+
+    cin >> T;
+    cin >> N >> K;
+    for(int l = 0; l < T; l++){
+        for(int i =0 ; i < N ; i++){
+            cin >> time;
+            v.push_back(time);
+        }
+        for (int s = 0; s < K; s++){
+            cin >> start >> end;
+            x.push_back(start);
+            x.push_back(end);
+            f.push_back(x);
+
+
+            x.clear()
+        }
+        cin >> vic;
+    }
+}
