1ca64bffbca289e92497783a419caa92d61a5215
pumaka <pumaka123@gmail.com>
2022-02-16 10:09:52 +0900

    5622 Test

---
+++ Baekjoon/5622.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    char A;
+    int time = 0;
+    cin >> s;
+    for ( int i = 0; i < s.size(); i++){
+        A = s[i];
+        if(int(A) >= int('S')){
+            A -= 1;
+        }
+        if(A == 'Z'){
+            A -= 1;
+        }
+        cout << int(A)-65 << " " << A << "\n";
+        time += ((int(A) - 65)/3)+3;
+    }
+    cout << time;
+    return 0;
+}
