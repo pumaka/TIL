8d136df8d70cdafd5d22a6e13e010ea0531357ca
pumaka <pumaka123@gmail.com>
2022-02-20 06:14:21 +0900

    22.02.19

---
+++ Baekjoon/11654_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    char A;
+    cin >> A;
+    cout << int(A);
+    return 0;
+}
---
+++ Baekjoon/2775.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int addition(int n, int k){
+    if(n<=0){
+        return k;
+    }
+    for(int i<1 ; i < k+1; i++){
+        addition(n-1, k);
+    }
+}
+
+int main() {
+    int n, k,T;
+    for(int i = 0; i < T; i++){
+
+
+    }
+
+    return 0;
+}
