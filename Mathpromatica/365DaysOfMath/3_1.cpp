ef19091cdf3b564f59085331c1d707f1b2d2130a
pumaka <pumaka123@gmail.com>
2022-03-01 13:06:04 +0900

    Getting started with math

---
+++ Mathpromatica/365DaysOfMath/3_1.cpp
@@ -0,0 +1,10 @@
+/*
+ * Euler's Equation
+ *
+ * The so called "most beautiful equation in the world"
+ * This equation acts as a bridge between "exponential functions" and "trignometric functions"
+ *
+ * This program will try to represent exponential funcitons in trignometric and vice versa.
+ * -Will also include visual representation of equation "exp(x) = sig((1/factorial(k))*(x**k)"
+ *
+ * /
