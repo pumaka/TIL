77df3607187708e5d11bd6c0bdaf91afa8a1c11d
pumaka <pumaka123@gmail.com>
2022-03-02 22:59:55 +0900

    22.3.2

--- Mathpromatica/365DaysOfMath/3_1.cpp
+++ Mathpromatica/365DaysOfMath/3_1.cpp
@@ -7,4 +7,14 @@
  * This program will try to represent exponential funcitons in trignometric and vice versa.
  * -Will also include visual representation of equation "exp(x) = sig((1/factorial(k))*(x**k)"
  *
- * /
+ */ 
+
+
+#include <cstdio>
+
+
+int e2c(int exponent, int power){
+	/* exponent has to be an integer, and the power should be multiplied by i(imaginary number)*/
+	 
+
+
