e5c5db8a927f9dd9fb85c66ddb95f1c154187f62
pumaka <pumaka123@gmail.com>
2022-07-24 10:51:32 +0900

    Update PossibleCombinations.cpp

--- AlgoRev/PossibleCombinations.cpp
+++ AlgoRev/PossibleCombinations.cpp
@@ -1,5 +1,6 @@
 /* ref: https://www.guru99.com/print-all-possible-combinations.html */
 
+// Method 1
 #include<bits/stdc++.h>
 #include<stdio.h>
 void Combination(char inputArray[], char combinationArray[], int start, int end, int index, int r) {
@@ -22,4 +23,30 @@ int main() {
   char combinationArray[r];
   printf("Combinations:\n");
   Combination(inputArray, combinationArray, 0, n - 1, 0, r);
+}
+
+// Method 2
+#include<bits/stdc++.h>
+#include<stdio.h>
+void Combination(char inputArray[], char combinationArray[], int n, int r, int index, int i) {
+  if (index == r) {
+    for (int j = 0; j & lt; r; j++) {
+      printf("%c", combinationArray[j]);
+    }
+    printf("\n");
+    return;
+  }
+  if (i & gt; = n)
+    return;
+  combinationArray[index] = inputArray[i];
+  Combination(inputArray, combinationArray, n, r, index + 1, i + 1);
+  Combination(inputArray, combinationArray, n, r, index, i + 1);
+}
+int main() {
+  char inputArray[] = {'R','G','Y','B','I'};
+  int n = sizeof(inputArray) / sizeof(inputArray[0]);
+  int r = 3;
+  char combinationArray[r];
+  printf("Combinations:\n");
+  Combination(inputArray, combinationArray, n, r, 0, 0);
 }
