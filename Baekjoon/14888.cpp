439f69f5e5f21e2d536f16b8f42a565a5ae8d354
pumaka <pumaka123@gmail.com>
2023-03-06 21:17:37 +0900

    23.03.06

--- Baekjoon/14888.cpp
+++ Baekjoon/14888.cpp
@@ -1,30 +1,69 @@
-
-#include <cstdio>
-#include <algorithm>
+#include <iostream>
 #include <vector>
 
+using namespace std;
+
+int mx = -1000000000;
+int mn = 1000000000;
+
+int ops(int a, int b, int op){
+    if(op == 1){
+        return a+b;
+    }
+    if(op == 2){
+        return a-b;
+    }
+    if(op == 3){
+        return a*b;
+    }
+    if(op == 4){
+        return a/b;
+    }
+    return b;
+}
+
+int solve(vector<int> num, vector<int> op, int val = 0){
+    vector<int> tmp_o, tmp_n;
+    int tmp;
+    
+    for(int i = 0; i < op.size(); i++){
+        tmp_n = num;
+        tmp_o = op;
+        tmp = ops(val, tmp_n[1], op[i]);
+        tmp_o.erase(tmp_o.begin()+i);
+        tmp_n.erase(tmp_n.begin());
+        solve(tmp_n , tmp_o, tmp);
+    }
+    if(op.size() == 0){
+    	if(mx < val){
+       	    mx = val;
+   	}
+        if(mn > val){
+            mn = val;
+        }
+    }
+    return 0;
+}
+
 int main() {
-    int N, x;
-    std::vector<int> v;
-    int oper[4] = {0};
-    for(int i = 0; i < N; i++){
-        scanf("%d", &x);
-        v.push_back(x);
+    int N, tmp;
+    vector<int> num, op;
+    
+    cin >> N;
+    for(int i = 0 ; i< N; i++){
+        cin >> tmp;
+        num.push_back(tmp);
     }
-    for(int i = 0; i < N; i++){
-        scanf("%d", &x);
-        oper[i];
+    for(int i = 1; i < 5; i++){
+        cin >> N;
+        for(int l = 0; l < N; l++){
+            op.push_back(i);
+        }
     }
     
-    std::sort(v.begin(), v.end());
+    solve(num, op, num[0]);
     
-    // largest
-    int largest = 0;
+    cout << mx << "\n" << mn << "\n";
     
-    
-    // smallest
-    int smallest = 0;
-
-
     return 0;
 }
---
+++ Baekjoon/14889.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+
+
+
+
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++){
+	for(int l = 0; l < N; l++){
+	    scanf("%d", &s[i][l]);
+	}
+    }
